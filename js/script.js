63333e489de73281b488a6fb8edf3ca256e80068
Lucian Aionicesei <lucian.aionicesei@gmail.com>
2021-12-08 15:57:24 +0100

    finished the logo animation

--- js/script.js
+++ js/script.js
@@ -1,19 +1,41 @@
-
 // *** DROPDOWN MENU EVENTS ***
 
 const items = document.querySelectorAll(".activate-dropdown");
+let heroInPosition = true;
 
 items.forEach(item => {
     item.addEventListener("mouseenter", expandLogo);
     item.addEventListener("mouseleave", backInPosition);
 });
 
-function expandLogo () {
+const heroImg = document.querySelector("#hero-img img");
+
+const callbackFunction = function (entry) {
+    console.log(entry[0].isIntersecting);
+    if (entry[0].isIntersecting == true) {
+        document.querySelector(".header-logo img").classList.remove("logo-current");
+        heroInPosition = true;
+    } else if (entry[0].isIntersecting == false) {
+        document.querySelector(".header-logo img").classList.add("logo-current");
+        heroInPosition = false;
+    }
+};
+
+const observer = new IntersectionObserver(callbackFunction, {
+    threshold: 1,
+    rootMargin: "-90px 0px 0px 0px"
+});
+
+observer.observe(heroImg);
+
+function expandLogo() {
     console.log("mouseenter");
-    document.querySelector(".header-logo img").classList.toggle("logo-current");
+    document.querySelector(".header-logo img").classList.add("logo-current");
 }
 
-function backInPosition () {
+function backInPosition() {
     console.log("mouseleave");
-    document.querySelector(".header-logo img").classList.toggle("logo-current");
+    if (heroInPosition == true) {
+        document.querySelector(".header-logo img").classList.remove("logo-current");
+    }
 }
