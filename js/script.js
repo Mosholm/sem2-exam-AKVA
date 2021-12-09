e328e92442d357c15e0b60074ab716d605d87fca
Lucian Aionicesei <lucian.aionicesei@gmail.com>
2021-12-09 10:41:55 +0100

    logo animation on width

--- js/script.js
+++ js/script.js
@@ -1,23 +1,49 @@
 // *** DROPDOWN MENU EVENTS ***
 
+// *** Report Window Size ***//
+
+let htmlElem = document.querySelector("html");
+// console.log(htmlElem.clientWidth);
+let windowWidth = htmlElem.clientWidth;
+
+window.onresize = reportWindowSize;
+
+function reportWindowSize() {
+    windowWidth = htmlElem.clientWidth;
+    // console.log(windowWidth);
+    if (windowWidth < 1000) {
+        document.querySelector(".header-logo img").classList.remove("logo-current");
+    } else if (windowWidth >= 1000 && heroInPosition == true) {
+        document.querySelector(".header-logo img").classList.add("logo-current");
+    } else if (windowWidth >= 1000 && heroInPosition == false){
+        document.querySelector(".header-logo img").classList.remove("logo-current");
+    }
+}
+
+// *** Dropdown Menu Functionalities ***///
+
 const items = document.querySelectorAll(".activate-dropdown");
 let heroInPosition = true;
 
 items.forEach(item => {
-    item.addEventListener("mouseenter", expandLogo);
-    item.addEventListener("mouseleave", backInPosition);
+    if (windowWidth >= 1000) {
+        item.addEventListener("mouseenter", expandLogo);
+        item.addEventListener("mouseleave", backInPosition);
+    }
 });
 
 const heroImg = document.querySelector("#hero-img img");
 
-const callbackFunction = function (entry) {
+function callbackFunction(entry) {
     console.log(entry[0].isIntersecting);
-    if (entry[0].isIntersecting == true) {
-        document.querySelector(".header-logo img").classList.remove("logo-current");
-        heroInPosition = true;
-    } else if (entry[0].isIntersecting == false) {
-        document.querySelector(".header-logo img").classList.add("logo-current");
-        heroInPosition = false;
+    if (windowWidth >= 1000) {
+        if (entry[0].isIntersecting == true) {
+            document.querySelector(".header-logo img").classList.add("logo-current");
+            heroInPosition = true;
+        } else if (entry[0].isIntersecting == false) {
+            document.querySelector(".header-logo img").classList.remove("logo-current");
+            heroInPosition = false;
+        }
     }
 };
 
@@ -29,13 +55,17 @@ const observer = new IntersectionObserver(callbackFunction, {
 observer.observe(heroImg);
 
 function expandLogo() {
-    console.log("mouseenter");
-    document.querySelector(".header-logo img").classList.add("logo-current");
+    // console.log("mouseenter");
+    if (windowWidth >= 1000) {
+        document.querySelector(".header-logo img").classList.remove("logo-current");
+    }
 }
 
 function backInPosition() {
-    console.log("mouseleave");
-    if (heroInPosition == true) {
-        document.querySelector(".header-logo img").classList.remove("logo-current");
+    // console.log("mouseleave");
+    if (windowWidth >= 1000) {
+        if (heroInPosition == true) {
+            document.querySelector(".header-logo img").classList.add("logo-current");
+        }
     }
 }
