dad36864ff3bf02b8b85815b860524368275ed52
George <geor2335@stud.kea.dk>
2021-12-09 13:57:04 +0100

    product-list 90% done

--- js/script.js
+++ js/script.js
@@ -10,15 +10,15 @@ window.onresize = reportWindowSize;
 const widthThreshold = 1100;
 
 function reportWindowSize() {
-    windowWidth = htmlElem.clientWidth;
-    // console.log(windowWidth);
-    if (windowWidth < widthThreshold) {
-        document.querySelector(".header-logo img").classList.remove("logo-current");
-    } else if (windowWidth >= widthThreshold && heroInPosition == true) {
-        document.querySelector(".header-logo img").classList.add("logo-current");
-    } else if (windowWidth >= widthThreshold && heroInPosition == false) {
-        document.querySelector(".header-logo img").classList.remove("logo-current");
-    }
+  windowWidth = htmlElem.clientWidth;
+  // console.log(windowWidth);
+  if (windowWidth < widthThreshold) {
+    document.querySelector(".header-logo img").classList.remove("logo-current");
+  } else if (windowWidth >= widthThreshold && heroInPosition == true) {
+    document.querySelector(".header-logo img").classList.add("logo-current");
+  } else if (windowWidth >= widthThreshold && heroInPosition == false) {
+    document.querySelector(".header-logo img").classList.remove("logo-current");
+  }
 }
 
 // *** Dropdown Menu Functionalities ***///
@@ -26,49 +26,51 @@ function reportWindowSize() {
 const items = document.querySelectorAll(".activate-dropdown");
 let heroInPosition = true;
 
-items.forEach(item => {
-    if (windowWidth >= widthThreshold) {
-        item.addEventListener("mouseenter", expandLogo);
-        item.addEventListener("mouseleave", backInPosition);
-    }
+items.forEach((item) => {
+  if (windowWidth >= widthThreshold) {
+    item.addEventListener("mouseenter", expandLogo);
+    item.addEventListener("mouseleave", backInPosition);
+  }
 });
 
 const heroImg = document.querySelector("#hero-img img");
 
 function callbackFunction(entry) {
-    console.log(entry[0].isIntersecting);
-    if (windowWidth >= widthThreshold) {
-        if (entry[0].isIntersecting == true) {
-            document.querySelector(".header-logo img").classList.add("logo-current");
-            heroInPosition = true;
-        } else if (entry[0].isIntersecting == false) {
-            document.querySelector(".header-logo img").classList.remove("logo-current");
-            heroInPosition = false;
-        }
+  console.log(entry[0].isIntersecting);
+  if (windowWidth >= widthThreshold) {
+    if (entry[0].isIntersecting == true) {
+      document.querySelector(".header-logo img").classList.add("logo-current");
+      heroInPosition = true;
+    } else if (entry[0].isIntersecting == false) {
+      document
+        .querySelector(".header-logo img")
+        .classList.remove("logo-current");
+      heroInPosition = false;
     }
-};
+  }
+}
 
 const observer = new IntersectionObserver(callbackFunction, {
-    threshold: 1,
-    rootMargin: "-90px 0px 80% 0px"
+  threshold: 1,
+  rootMargin: "-90px 0px 80% 0px",
 });
 
 observer.observe(heroImg);
 
 function expandLogo() {
-    // console.log("mouseenter");
-    if (windowWidth >= widthThreshold) {
-        document.querySelector(".header-logo img").classList.remove("logo-current");
-    }
+  // console.log("mouseenter");
+  if (windowWidth >= widthThreshold) {
+    document.querySelector(".header-logo img").classList.remove("logo-current");
+  }
 }
 
 function backInPosition() {
-    // console.log("mouseleave");
-    if (windowWidth >= widthThreshold) {
-        if (heroInPosition == true) {
-            document.querySelector(".header-logo img").classList.add("logo-current");
-        }
+  // console.log("mouseleave");
+  if (windowWidth >= widthThreshold) {
+    if (heroInPosition == true) {
+      document.querySelector(".header-logo img").classList.add("logo-current");
     }
+  }
 }
 
 const collections = document.querySelectorAll(".cover-coll");
@@ -76,17 +78,17 @@ const link1 = document.querySelector("#col-link1");
 const link2 = document.querySelector("#col-link2");
 
 collections[0].addEventListener("mouseenter", () => {
-    link1.style.setProperty("--default-line-width", "0");
+  link1.style.setProperty("--default-line-width", "0");
 });
 
 collections[0].addEventListener("mouseleave", () => {
-    link1.style.setProperty("--default-line-width", "100%");
+  link1.style.setProperty("--default-line-width", "100%");
 });
 
 collections[1].addEventListener("mouseenter", () => {
-    link2.style.setProperty("--default-line-width", "0");
+  link2.style.setProperty("--default-line-width", "0");
 });
 
 collections[1].addEventListener("mouseleave", () => {
-    link2.style.setProperty("--default-line-width", "100%");
+  link2.style.setProperty("--default-line-width", "100%");
 });
